864a1217bbaa83769f9a258a28f391a862fb90db
Amirreza Soheiliarasi <soheilyamirreza@yahoo.com>
2026-01-04 14:59:12 +0100

    Update App.js

--- DangiDongiByAmirreza/src/App.js
+++ DangiDongiByAmirreza/src/App.js
@@ -233,11 +233,10 @@ function App() {
                 {results.results.map((transaction, index) => (
                   <motion.li key={index} custom={index} variants={listItemVariants} initial="hidden" animate="visible" className="transaction-item">
                     {/* Logic update for LTR and text translation */}
-                    <span className={`pays-money ${transaction.from.includes('(Each)') ? 'unpaid-group-text' : ''}`}>
+                    <span className={`pays-money ${transaction.from.includes('(Each)') ? 'unpaid-group-text' : ''}`}></span>
                     <span className="arrow"><FiArrowRight /><span className="amount">{formatNumber(transaction.amount, currency)} {currency}</span></span>
                     <span className="gets-money">{transaction.to}</span>
-                      {transaction.from}
-                    </span>
+                {transaction.from}
                   </motion.li>
                 ))}
               </ul>
